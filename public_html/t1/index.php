3f7808f9743e9ad1090eb603d2dc3dff61a96fdd
Renato Gomes <renatoo.agomes@gmail.com>
2016-04-30 14:28:48 -0300

    commit com tangente, sec, e cosec

--- public_html/t1/index.php
+++ public_html/t1/index.php
@@ -3,7 +3,7 @@ require "t1.php";
 
 //inicializando variaveis
 $argumento = 3;
-$precisao = 0.001;
+$precisao = 0.0001;
 $maxIteracoes = 50;
 $numIteracoes = 1;
 $codErro = 0;
@@ -14,41 +14,70 @@ $codErro = 0;
     <?php require "../head.html" ?>
     <body>
         <h1>Metodos numericos computacionais</h1>
-            <div class="row">
+        <div class="row">
             <div class="col-sm-8">
                 <ul class="list list-group">
                     <li class="list-group-item">
                         <a href="../"> Voltar </a>
                     </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#expo">Funcao Expo</button>
+                        </button>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#loge">Funcao Loge</button>
+                        </button>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#seno">Funcao Seno</button>
+                        </button>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#cosseno">Funcao Cosseno</button>
+                        </button>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#tangente">Funcao Tangente</button>
+                        </button>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#secante">Funcao Secante</button>
+                        </button>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#cosecante">Funcao Cosecante</button>
+                        </button>
+                    </li>
+
                 </ul>
             </div>
 
-                <div class="col-sm-8">
-                    <h2>Calculando e^<?php echo $argumento ?></h2>
-                    <table class="table">
-                        <tr>
-                            <th>K (&iacute;ndice)</th>
-                            <th>Termo</th>
-                            <th>Exponencial</th>
-                            <th>Erro</th>
-                        </tr>
-                        <?php imprimeExpoIteracoes(0, 1, 1, "---", $precisao); ?>
-                        <?php expo($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
-                    </table>
-                </div>
+            <div id="expo" class="col-sm-8 collapse">
+                <h2>Calculando e^<?php echo $argumento ?></h2>
+                <table class="table">
+                    <tr>
+                        <th>K (&iacute;ndice)</th>
+                        <th>Termo</th>
+                        <th>Exponencial</th>
+                        <th>Erro</th>
+                    </tr>
+                    <?php $valorExpo = expo($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                    <?php imprimeIteracoes($numIteracoes, $valorExpo, "---", "---", arrumaPrecisao($precisao)); ?>
+                </table>
             </div>
+        </div>
 
 <?php
 //inicializando variaveis
 $argumento = 5;
-$precisao = 0.01;
+$precisao = 0.0001;
 $maxIteracoes = 50;
 $numIteracoes = 1;
 $codErro = 0;
 ?>
 
             <div class="row">
-                <div class="col-sm-8">
+                <div id="loge" class="collapse col-sm-8">
                     <table class="table">
                     <h2>Calculando ln <?php echo $argumento ?></h2>
                         <tr>
@@ -57,14 +86,15 @@ $codErro = 0;
                             <th>Logaritmica</th>
                             <th>Erro</th>
                         </tr>
-                        <?php loge($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php $valorLoge = loge($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php imprimeIteracoes($numIteracoes, $valorLoge, "---", "---", arrumaPrecisao($precisao)); ?>
                     </table>
                 </div>
             </div>
 
 <?php
 //inicializando variaveis
-$argumento = 57.59586;
+$argumento = 1.0471975511966;
 $precisao = 0.01;
 $maxIteracoes = 50;
 $numIteracoes = 1;
@@ -72,7 +102,7 @@ $codErro = 0;
 ?>
 
             <div class="row">
-                <div class="col-sm-8">
+                <div id="seno" class="collapse col-sm-8">
                     <table class="table">
                        <h2>Calculando sen <?php echo $argumento ?> = sen <?php echo number_format(getValorSemVoltasCompletas($argumento), arrumaPrecisao($precisao));
             ?></h2>
@@ -82,22 +112,23 @@ $codErro = 0;
                             <th>Seno</th>
                             <th>Erro</th>
                         </tr>
-                        <?php seno($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php $valorSeno =  seno($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php imprimeIteracoes($numIteracoes, $valorSeno, "---", "---", arrumaPrecisao($precisao)); ?>
                     </table>
                 </div>
             </div>
 
 <?php
 //inicializando variaveis
-$argumento = ((47*M_PI)/2);
-$precisao = 0.01;
+$argumento = 1.0471975511966;
+$precisao = 0.0001;
 $maxIteracoes = 50;
 $numIteracoes = 2;
 $codErro = 0;
 ?>
 
             <div class="row">
-                <div class="col-sm-8">
+                <div id="cosseno" class="collapse col-sm-8">
                     <table class="table">
                        <h2>Calculando cos <?php echo $argumento ?> = cos <?php echo number_format(getValorSemVoltasCompletas($argumento), arrumaPrecisao($precisao));
             ?></h2>
@@ -107,10 +138,93 @@ $codErro = 0;
                             <th>Cosseno</th>
                             <th>Erro</th>
                         </tr>
-                        <?php cosseno($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php $valorCosseno = cosseno($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php imprimeIteracoes($numIteracoes, $valorCosseno, "---", "---", arrumaPrecisao($precisao)); ?>
                     </table>
                 </div>
         </div>
+<?php
+//inicializando variaveis
+$argumento = 1.0471975511966;
+$precisao = 0.0001;
+$maxIteracoes = 50;
+$numIteracoes = 0;
+$codErro = 0;
+?>
+
+            <div class="row">
+                <div id="tangente" class="collapse col-sm-8">
+                    <table class="table">
+                       <h2>Calculando tan <?php echo $argumento ?> = tan <?php echo number_format(getValorSemVoltasCompletas($argumento), arrumaPrecisao($precisao)); ?></h2>
+                       <tr>
+                            <th>K (&iacute;ndice)</th>
+                            <th>Termo</th>
+                            <th>Tangente</th>
+                            <th>Erro</th>
+                        </tr>
+                        <?php $valorTangente =  tag($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php imprimeIteracoes($numIteracoes, $valorTangente, "---", "---", arrumaPrecisao($precisao)); ?>
+                    </table>
+                </div>
+            </div>
+
+<?php
+//inicializando variaveis
+$argumento = ((34*M_PI)/2);
+$precisao = 0.0001;
+$maxIteracoes = 50;
+$numIteracoes = 0;
+$codErro = 0;
+?>
+
+            <div class="row">
+                <div id="secante" class="collapse col-sm-8">
+                    <table class="table">
+                       <h2>Calculando sec <?php echo $argumento ?> = sec <?php echo number_format(getValorSemVoltasCompletas($argumento), arrumaPrecisao($precisao)); ?></h2>
+                       <tr>
+                            <th>K (&iacute;ndice)</th>
+                            <th>Termo</th>
+                            <th>Secante</th>
+                            <th>Erro</th>
+                        </tr>
+                        <?php $valorSecante =  sec($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php imprimeIteracoes($numIteracoes, $valorSecante, "---", "---", arrumaPrecisao($precisao)); ?>
+                    </table>
+                </div>
+            </div>
+
+
+<?php
+//inicializando variaveis
+$argumento = M_PI/6;
+$precisao = 0.0001;
+$maxIteracoes = 50;
+$numIteracoes = 0;
+$codErro = 0;
+?>
+
+            <div class="row">
+                <div id="cosecante" class="collapse col-sm-8">
+                    <table class="table">
+                       <h2>Calculando cosec <?php echo $argumento ?> = cosec <?php echo number_format(getValorSemVoltasCompletas($argumento), arrumaPrecisao($precisao)); ?></h2>
+                       <tr>
+                            <th>K (&iacute;ndice)</th>
+                            <th>Termo</th>
+                            <th>Cosecante</th>
+                            <th>Erro</th>
+                        </tr>
+                        <?php $valorCosecante =  cosec($argumento, $precisao, $maxIteracoes, $numIteracoes, $codErro); ?>
+                        <?php imprimeIteracoes($numIteracoes, $valorCosecante, "---", "---", arrumaPrecisao($precisao)); ?>
+                    </table>
+                </div>
+            </div>
+
+
+
+
+
+
+
     </body>
 </html>
 
--- public_html/t1/t1.php
+++ public_html/t1/t1.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Metodo para calcular o valor de uma funcao exponencial
@@ -9,7 +9,7 @@
  * @param $numIteraÃ§Ãµes - retorno com o numero de iteraÃ§Ãµes realizadas
  * @param $codErro - retorno representando sucesso ou nao
  *
- * @return $codErro da funcao no ponto 
+ * @return $codErro da funcao no ponto
  */
 function expo($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
 {
@@ -32,7 +32,7 @@ function expo($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
         $termo =  number_format($termo, $casasDecimais);
         $valor =  number_format($valor, $casasDecimais);
 
-        imprimeExpoIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
+        //imprimeIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
 
         $numIteracoes++;
 
@@ -48,44 +48,7 @@ function expo($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
         }
     }
 
-    return $codErro;
-}
-
-/**
- * Metodo para imprimir uma iteracao do metodo expo em forma de tr
- *
- * @param $indiceIteracao - numero da iteracao
- * @param $valorExponencial - valor da funcao Exponencial para essa iteracao
- * @param $valorTermo - valor do termo para essa iteracao
- * @param $valorErro - valor do erro para essa iteracao
- * @param $precisao - numero de casas decimais depois da virgula
- */
-function imprimeExpoIteracoes($indiceIteracao, $valorExponencial, $valorTermo, $valorErro, $precisao)
-{
-    echo "<tr>" .
-            "<td>$indiceIteracao</td>" .
-            "<td>".number_format($valorTermo, $precisao)."</td>" .
-            "<td>".number_format($valorExponencial, $precisao)."</td>" .
-            "<td>".((is_numeric($valorErro)) ? number_format($valorErro, $precisao+1) : $valorErro). "</td>" .
-        "</tr>";
-}
-
-/**
- * Metodo para arrumar a precisao (0.001) --> (3)
- *
- * @param $precisao - numero para a precisao (0.0001)
- *
- * @return numero de casas decimais
- */
-function arrumaPrecisao($precisao) {
-    //se existir uma string depois do '.' entao pegue o numero de caracteres
-    if (array_key_exists("1", explode('.', $precisao))) {
-        $precisao =  strlen(explode('.', $precisao)[1]);
-    } else {
-        $precisao = 3;
-    }
-
-    return ($precisao+1);
+    return $valor;
 }
 
 
@@ -121,7 +84,7 @@ function loge($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
         $termo =  number_format($termo, $casasDecimais);
         $valor =  number_format($valor, $casasDecimais);
 
-        imprimeExpoIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
+        //imprimeIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
 
         $numIteracoes++;
         $numIteracoes++;
@@ -138,7 +101,7 @@ function loge($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
         }
     }
 
-    return $codErro;
+    return $valor;
 }
 
 /**
@@ -176,7 +139,7 @@ function seno($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
 
         //formatando os valores de acordo com a precisao desejada
 
-        imprimeExpoIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
+       //imprimeIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
 
         $numIteracoes++;
         $numIteracoes++;
@@ -193,7 +156,7 @@ function seno($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
         }
     }
 
-    return $codErro;
+    return $valor;
 }
 
 /**
@@ -215,7 +178,7 @@ function cosseno($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro
     $argumento = getValorSemVoltasCompletas($argumento);
 
     //inicializando variaveis para o loop
-    $condicaoParada = true;
+    $condicaoParada = false;
     $valor = 1;
     $termo = 1;
     $erro = 1;
@@ -224,14 +187,14 @@ function cosseno($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro
     while (!$condicaoParada) {
         //calculando os novos valores para essa iteracao
         if ($numIteracoes > 1) {
-           //TODO: qual razao para ser multiplicada? $termo = 
+            $termo = $termo * ( (-pow($argumento,2) ) / (($numIteracoes-1)*($numIteracoes)) );
         }
         $valor = $valor + $termo;
         $erro = abs(($numIteracoes > 1) ? ($termo / $valor) : 1);
 
         //formatando os valores de acordo com a precisao desejada
 
-        imprimeExpoIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
+       //imprimeIteracoes($numIteracoes, $valor, $termo, $erro, $casasDecimais);
 
         $numIteracoes++;
         $numIteracoes++;
@@ -248,9 +211,138 @@ function cosseno($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro
         }
     }
 
-    return $codErro;
+    return $valor;
 }
 
+/**
+ * Metodo para calcular o valor de uma funcao tangente
+ *
+ * @param $argumento - valor de x
+ * @param $precisao - valor da precisao desejada (ex: 0.0001 = 10^-4)
+ * @param $maxIteracoes - numero maximo de iteraÃ§Ãµes
+ * @param $numIteraÃ§Ãµes - retorno com o numero de iteraÃ§Ãµes realizadas
+ * @param $codErro - retorno representando sucesso ou nao
+ *
+ * @return $codErro da funcao no ponto
+ */
+function tag($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
+{
+    //removendo as voltas completas do argumento para que os valores
+    //estejam dentro do intervalo [0 - 2PI]
+    $argumento = getValorSemVoltasCompletas($argumento);
+
+    //arrumando parametro precisao
+    $casasDecimais = arrumaPrecisao($precisao);
+
+    //inicializando os indices de Seno e Cosseno, para que as iteracoes deles
+    //nao comecem com indices errados
+    $numIteracoesSeno = 1;
+    $numIteracoesCosseno = 2;
+
+    //variaveis para pegar o codErro das funcoes seno e cosseno
+    $codErroSeno = 0;
+    $codErroCosseno = 0;
+
+    //Calculando tan x = sen x / cos x
+    $senoNoPto = seno($argumento, $precisao, $maxIteracoes, $numIteracoesSeno, $codErroSeno);
+    $cossenoNoPto = cosseno($argumento, $precisao, $maxIteracoes, $numIteracoesCosseno, $codErroCosseno);
+    $valor = $senoNoPto / $cossenoNoPto;
+
+    // no caso de tangente, tenho que checar os códigos de erro das funcoes seno e cosseno
+    // ja que nao uso da formula de series de tangente e sim das series de seno e cosseno
+    if (!$codErroSeno || !$codErroCosseno) {
+        //como 0 é o codigo de sucesso, se ambos os códigos de erro forem 0 entao funcionou
+        $codErro = 0;
+    } else {
+        $codErro = $codErroSeno ? $codErroSeno : $codErroCosseno;
+    }
+
+    return $valor;
+}
+
+/**
+ * Metodo para calcular o valor de uma funcao secante
+ *
+ * @param $argumento - valor de x
+ * @param $precisao - valor da precisao desejada (ex: 0.0001 = 10^-4)
+ * @param $maxIteracoes - numero maximo de Iteracoes
+ * @param $numIteracoes - retorno com o numero de Iteracoes realizadas
+ * @param $codErro - retorno representando sucesso ou nao
+ *
+ * @return $valor - valor da funcao secante no ponto
+ */
+function sec($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
+{
+    //removendo as voltas completas do argumento para que os valores
+    //estejam dentro do intervalo [0 - 2PI]
+    $argumento = getValorSemVoltasCompletas($argumento);
+
+    //arrumando parametro precisao
+    $casasDecimais = arrumaPrecisao($precisao);
+
+    //inicializando os indice de Cosseno, para que as iteracoes dele
+    //nao comecem com indices errados
+    $numIteracoesCosseno = 2;
+
+    //variaveis para pegar o codErro da funcao cosseno
+    $codErroCosseno = 0;
+
+    //Calculando sec x =  1 / cos x
+    $cossenoNoPto = cosseno($argumento, $precisao, $maxIteracoes, $numIteracoesCosseno, $codErroCosseno);
+    $valor = 1 / $cossenoNoPto;
+
+    // no caso de secante, tenho que checar o código de erro da funcao cosseno
+    if (!$codErroCosseno) {
+        //como 0 é o codigo de sucesso, se ambos os códigos de erro forem 0 entao funcionou
+        $codErro = 0;
+    } else {
+        $codErro = $codErroCosseno;
+    }
+
+    return $valor;
+}
+
+/**
+ * Metodo para calcular o valor de uma funcao cosecante
+ *
+ * @param $argumento - valor de x
+ * @param $precisao - valor da precisao desejada (ex: 0.0001 = 10^-4)
+ * @param $maxIteracoes - numero maximo de Iteracoes
+ * @param $numIteracoes - retorno com o numero de Iteracoes realizadas
+ * @param $codErro - retorno representando sucesso ou nao
+ *
+ * @return $valor - valor da funcao cosecante no ponto
+ */
+function cosec($argumento, $precisao, $maxIteracoes, &$numIteracoes, &$codErro)
+{
+    //removendo as voltas completas do argumento para que os valores
+    //estejam dentro do intervalo [0 - 2PI]
+    $argumento = getValorSemVoltasCompletas($argumento);
+
+    //arrumando parametro precisao
+    $casasDecimais = arrumaPrecisao($precisao);
+
+    //inicializando os indice de Seno, para que as iteracoes dele
+    //nao comecem com indices errados
+    $numIteracoesSeno = 1;
+
+    //variaveis para pegar o codErro das funcoes seno
+    $codErroSeno = 0;
+
+    //Calculando cosec x =  1 / sen x
+    $senoNoPto = seno($argumento, $precisao, $maxIteracoes, $numIteracoesSeno, $codErroSeno);
+    $valor = 1 / $senoNoPto;
+
+    // no caso de cosecante, tenho que checar o código de erro da funcao seno
+    if (!$codErroSeno) {
+        //como 0 é o codigo de sucesso, se ambos os códigos de erro forem 0 entao funcionou
+        $codErro = 0;
+    } else {
+        $codErro = $codErroSeno;
+    }
+
+    return $valor;
+}
 
 
 /**
@@ -273,5 +365,46 @@ function getValorSemVoltasCompletas($numRadianos)
 
     return $fullValue;
 }
+
+/**
+ * Metodo para imprimir uma iteracao do metodo expo em forma de tr
+ *
+ * @param $indiceIteracao - numero da iteracao
+ * @param $valorExponencial - valor da funcao Exponencial para essa iteracao
+ * @param $valorTermo - valor do termo para essa iteracao
+ * @param $valorErro - valor do erro para essa iteracao
+ * @param $precisao - numero de casas decimais depois da virgula
+ */
+function imprimeIteracoes($indiceIteracao, $valorExponencial, $valorTermo, $valorErro, $precisao)
+{
+    echo "<tr>" .
+            "<td>$indiceIteracao</td>" .
+            "<td>".number_format($valorTermo, $precisao)."</td>" .
+            "<td>".number_format($valorExponencial, $precisao)."</td>" .
+            "<td>".((is_numeric($valorErro)) ? number_format($valorErro, $precisao+1) : $valorErro). "</td>" .
+        "</tr>";
+}
+
+
+
+/**
+ * Metodo para arrumar a precisao (0.001) --> (3)
+ *
+ * @param $precisao - numero para a precisao (0.0001)
+ *
+ * @return numero de casas decimais
+ */
+function arrumaPrecisao($precisao)
+{
+    //se existir uma string depois do '.' entao pegue o numero de caracteres
+    if (array_key_exists("1", explode('.', $precisao))) {
+        $precisao =  strlen(explode('.', $precisao)[1]);
+    } else {
+        $precisao = 3;
+    }
+
+    return ($precisao+1);
+}
+
 
 ?>
