10e1098d3783f83bd8d9eac13738a8ec77a0c2f9
Renato Gomes <renatoo.agomes@gmail.com>
2016-05-11 22:19:34 -0300

    Commit com comeco do t2

---
+++ public_html/t2/index.php
@@ -0,0 +1,45 @@
+<?php
+require "t2.php";
+
+$limInf = 0;
+$limSup = 3;
+
+?>
+
+<html>
+    <?php //require "../head.html" ?>
+    <body>
+        <h1>Metodos numericos computacionais</h1>
+        <div class="row">
+            <div class="col-sm-2 float-left">
+                <ul class="list list-group">
+                    <li class="list-group-item">
+                        <a href="../"> Voltar </a>
+                    </li>
+                    <li class="list-group-item">
+                       <button data-toggle="collapse" data-target="#bissecao">Funcao Bissecao</button>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="bissecao" class="col-sm-9 float-right collapse">
+                <h2>Calculando Zero de funcao pelo metodo da Bissecao<h2>
+                <table class="table">
+                    <tr>
+                        <th>K (&iacute;ndice)</th>
+                        <th>Termo</th>
+                        <th>Exponencial</th>
+                        <th>Erro</th>
+                    </tr>
+                    <?php
+                    $t2obj= new T2();
+                    $t2obj->bissecao($funcao, $limInf, $limSup, $precisao, $numIteracoes, $codErro);
+                    //imprimindo iteracoes
+
+                    ?>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ public_html/t2/t2.php
@@ -0,0 +1,85 @@
+<?php
+require "../funcoes_auxiliares.php";
+
+$funcao = function ($x) {
+    return 3 * $x - 2;
+};
+
+/**
+ * Classe para representar o T2 (Calculo de zeros de Funcao);
+ */
+Class T2
+{
+
+    /**
+     * Metodo para calcular zeros de funcao utilizado o metodo da bissecao
+     *
+     * @param $funcao - Closure - funcao que vamos encontrar a raiz
+     * @param $limInf - Limite inferior do intervalo inicial
+     * @param $limSup - Limite superior do intervalo inicial
+     * @param $precisao - valor da precisao desejada (ex: 0.0001 = 10^-4)
+     * @param $numIteracoes - retorno com o numero de iteracoes realizadas
+     * @param $codErro - retorno representando sucesso ou nao
+     *
+     * @return $valor da raiz da funcao
+     */
+    public function bissecao(Closure $funcao, $limInf, $limSup, $precisao, &$numIteracoes, &$codErro)
+    {
+        //arrumando casas decimais
+        $casasDecimais = arrumaPrecisao($precisao);
+
+
+        $tamanhoIntervalo = $limSup - $limInf;
+        while ($tamanhoIntervalo > $precisao)
+        {
+            //obtendo ponto medio do intervalo
+            $ptoMedio = ($limSup + $limInf) / 2;
+
+            //obtendo valores da funcao no ponto
+            $valorFuncaoInf = $funcao($limInf);
+            $valorFuncaoSup = $funcao($limSup);
+            $valorFuncaoPtoMedio = $funcao($ptoMedio);
+
+            //Testando pelo teorema de bolzano se existe raiz f(a)*f(b) < 0
+            $existeRaiz =  (($valorFuncaoInf * $valorFuncaoSup < 0) ? true : false );
+
+            if ($valorFuncaoInf == 0) {
+                echo 'intervalo inferior é raiz da funcao';
+                return $valorFuncaoInf;
+            }
+
+            if ($valorFuncaoSup == 0) {
+                echo 'intervalo superior é raiz da funcao';
+                return $valorFuncaoSup;
+            }
+
+            if ($valorFuncaoPtoMedio == 0) {
+                echo 'Raiz: ' . $ptoMedio;
+                return $valorFuncaoPtoMedio;
+            }
+
+            //se f(a) * f(x) < 0 (raiz estano intervalo da esquerda)
+            if ($valorFuncaoInf * $valorFuncaoPtoMedio < 0) {
+                $limSup = $ptoMedio;
+            }
+
+            //se f(b) * f(x) < 0 (raiz estano intervalo da direita)
+            if ($valorFuncaoSup * $valorFuncaoPtoMedio < 0) {
+                $limInf = $ptoMedio;
+            }
+
+            $numIteracoes++;
+        }
+   }
+
+
+
+
+
+}
+
+
+
+
+
+?>
